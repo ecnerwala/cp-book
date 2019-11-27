7e22aab0e32da5443da7d91fa63aadee39ccdd21
Andrew He <he.andrew.mail@gmail.com>
2019-11-26 20:20:08 -0800

    Add compile_flags and modnum

---
+++ src/modnum.hpp
@@ -0,0 +1,143 @@
+#pragma once
+
+#include <cassert>
+#include <iostream>
+
+template <int MOD_> struct modnum {
+	static constexpr int MOD = MOD_;
+	static_assert(MOD_ > 0, "MOD must be positive");
+
+private:
+	using ll = long long;
+
+	int v;
+
+	static int minv(int a, int m) {
+		a %= m;
+		assert(a);
+		return a == 1 ? 1 : int(m - ll(minv(m, a)) * ll(m) / a);
+	}
+
+public:
+
+	modnum() : v(0) {}
+	modnum(ll v_) : v(int(v_ % MOD)) { if (v < 0) v += MOD; }
+	explicit operator int() const { return v; }
+	friend std::ostream& operator << (std::ostream& out, const modnum& n) { return out << int(n); }
+	friend std::istream& operator >> (std::istream& in, modnum& n) { ll v_; in >> v_; n = modnum(v_); return in; }
+
+	friend bool operator == (const modnum& a, const modnum& b) { return a.v == b.v; }
+	friend bool operator != (const modnum& a, const modnum& b) { return a.v != b.v; }
+
+	modnum inv() const {
+		modnum res;
+		res.v = minv(v, MOD);
+		return res;
+	}
+	modnum neg() const {
+		modnum res;
+		res.v = v ? MOD-v : 0;
+		return res;
+	}
+
+	modnum operator- () const {
+		return neg();
+	}
+	modnum operator+ () const {
+		return modnum(*this);
+	}
+
+	modnum& operator ++ () {
+		v ++;
+		if (v == MOD) v = 0;
+		return *this;
+	}
+	modnum& operator -- () {
+		if (v == 0) v = MOD;
+		v --;
+		return *this;
+	}
+	modnum& operator += (const modnum& o) {
+		v += o.v;
+		if (v >= MOD) v -= MOD;
+		return *this;
+	}
+	modnum& operator -= (const modnum& o) {
+		v -= o.v;
+		if (v < 0) v += MOD;
+		return *this;
+	}
+	modnum& operator *= (const modnum& o) {
+		v = int(ll(v) * ll(o.v) % MOD);
+		return *this;
+	}
+	modnum& operator /= (const modnum& o) {
+		return *this *= o.inv();
+	}
+
+	friend modnum operator ++ (modnum& a, int) { modnum r = a; ++a; return r; }
+	friend modnum operator -- (modnum& a, int) { modnum r = a; --a; return r; }
+	friend modnum operator + (const modnum& a, const modnum& b) { return modnum(a) += b; }
+	friend modnum operator - (const modnum& a, const modnum& b) { return modnum(a) -= b; }
+	friend modnum operator * (const modnum& a, const modnum& b) { return modnum(a) *= b; }
+	friend modnum operator / (const modnum& a, const modnum& b) { return modnum(a) /= b; }
+};
+
+template <typename T> T pow(T a, long long b) {
+	assert(b >= 0);
+	T r = 1; while (b) { if (b & 1) r *= a; b >>= 1; a *= a; } return r;
+}
+
+template <typename U, typename V> struct pairnum {
+	U u;
+	V v;
+
+	pairnum() : u(0), v(0) {}
+	pairnum(long long val) : u(val), v(val) {}
+	pairnum(const U& u_, const V& v_) : u(u_), v(v_) {}
+
+	friend std::ostream& operator << (std::ostream& out, const pairnum& n) { return out << '(' << n.u << ',' << ' ' << n.v << ')'; }
+	friend std::istream& operator >> (std::istream& in, pairnum& n) { long long val; in >> val; n = pairnum(val); return in; }
+
+	friend bool operator == (const pairnum& a, const pairnum& b) { return a.u == b.u && a.v == b.v; }
+	friend bool operator != (const pairnum& a, const pairnum& b) { return a.u != b.u || a.v != b.v; }
+
+	pairnum inv() const {
+		return pairnum(u.inv(), v.inv());
+	}
+	pairnum neg() const {
+		return pairnum(u.neg(), v.neg());
+	}
+	pairnum operator- () const {
+		return pairnum(-u, -v);
+	}
+	pairnum operator+ () const {
+		return pairnum(+u, +v);
+	}
+
+	pairnum& operator += (const pairnum& o) {
+		u += o.u;
+		v += o.v;
+		return *this;
+	}
+	pairnum& operator -= (const pairnum& o) {
+		u -= o.u;
+		v -= o.v;
+		return *this;
+	}
+	pairnum& operator *= (const pairnum& o) {
+		u *= o.u;
+		v *= o.v;
+		return *this;
+	}
+	pairnum& operator /= (const pairnum& o) {
+		u /= o.u;
+		v /= o.v;
+		return *this;
+	}
+
+	friend pairnum operator + (const pairnum& a, const pairnum& b) { return pairnum(a) += b; }
+	friend pairnum operator - (const pairnum& a, const pairnum& b) { return pairnum(a) -= b; }
+	friend pairnum operator * (const pairnum& a, const pairnum& b) { return pairnum(a) *= b; }
+	friend pairnum operator / (const pairnum& a, const pairnum& b) { return pairnum(a) /= b; }
+};
