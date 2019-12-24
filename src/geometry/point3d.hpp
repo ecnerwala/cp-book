359da2bd2048c5452c0c38f765f5b55cc12332c2
Andrew He <he.andrew.mail@gmail.com>
2019-12-24 13:39:24 -0800

    Add point3d

---
+++ src/geometry/point3d.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include<bits/stdc++.h>
+
+const double PI = acos(-1.);
+const double TAU = 2 * PI;
+
+template <typename T> struct Point3D {
+	using P = Point3D;
+
+	T x, y, z;
+	Point3D() : x(0), y(0), z(0) {}
+	Point3D(T x_, T y_, T z_) : x(x_), y(y_), z(z_) {}
+
+	template <typename U> explicit Point3D(const Point3D<U>& p) : x(T(p.x)), y(T(p.y)), z(T(p.z)) {}
+
+	friend std::istream& operator >> (std::istream& i, P& p) { return i >> p.x >> p.y >> p.z; }
+	friend std::ostream& operator << (std::ostream& o, const P& p) { return o << "(" << p.x << "," << p.y << "," << p.z << ")"; }
+
+	friend bool operator == (const P& a, const P& b) { return a.x == b.x && a.y == b.y && a.z == b.z; }
+	friend bool operator != (const P& a, const P& b) { return a.x != b.x || a.y != b.y || a.z != b.z; }
+
+	P& operator += (const P& o) { x += o.x, y += o.y, z += o.z; return *this; }
+	P& operator -= (const P& o) { x -= o.x, y -= o.y, z -= o.z; return *this; }
+	friend P operator + (const P& a, const P& b) { return P(a) += b; }
+	friend P operator - (const P& a, const P& b) { return P(a) -= b; }
+
+	P& operator *= (const T& t) { x *= t, y *= t, z *= t; return *this; }
+	P& operator /= (const T& t) { x /= t, y /= t, z /= t; return *this; }
+	friend P operator * (const P& p, const T& t) { return P(p) *= t; }
+	friend P operator * (const T& t, const P& p) { return P(p) *= t; }
+	friend P operator / (const P& a, const T& t) { return P(a) /= t; }
+
+	friend P operator + (const P& a) { return P(+a.x, +a.y, +a.z); }
+	friend P operator - (const P& a) { return P(-a.x, -a.y, -a.z); }
+
+	friend T dot(const P& a, const P& b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
+	friend T norm(const P& a) { return dot(a,a); }
+	friend P cross(const P& a, const P& b) { return P(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x); }
+
+	friend T int_norm(const P& p) {
+		return gcd(gcd(abs(p.x), abs(p.y)), abs(p.z));
+	}
+	friend P int_unit(const P& p) {
+		T g = int_norm(p);
+		return g ? p / g : p;
+	}
+
+	friend T abs(const P& a) { return sqrt(max(T(0), norm(a))); }
+	friend P unit(const P& a) { return a / abs(a); }
+
+	friend T vol(const P& a, const P& b, const P& c, const P& d) { return dot(cross(b-a, c-a), d-a); }
+
+	friend bool lexLess(const P& a, const P& b) { return tie(a.x, a.y, a.z) < tie(b.x, b.y, b.z); }
+
+	friend bool parallelSame(const P& a, const P& b) {
+		assert(a != P());
+		assert(b != P());
+		return lexLess(a, P()) == lexLess(b, P());
+	}
+};
