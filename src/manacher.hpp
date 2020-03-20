c559b119f62daee90a6aa2f870f5173a15a6279d
Andrew He <he.andrew.mail@gmail.com>
2020-03-19 17:48:39 -0700

    Consolidate manacher

--- src/manacher.hpp
+++ src/manacher.hpp
@@ -16,19 +16,18 @@
 template <typename V> std::vector<int> manacher(const V& S) {
 	int N = int(S.size());
 	std::vector<int> res(2*N+1, 0);
-	int c = 0, r = 0;
-	for (int i = 1; i < 2*N; i++) {
+	for (int i = 1, j = -1, r = 0; i < 2*N; i++, j--) {
 		if (i > r) {
 			r = i+1, res[i] = 1;
 		} else {
-			res[i] = res[2*c-i];
+			res[i] = res[j];
 		}
 		if (i+res[i] >= r) {
 			int b = r>>1, a = i-b;
 			while (a > 0 && b < N && S[a-1] == S[b]) {
 				a--, b++;
 			}
-			res[i] = b-a, c = i, r = b<<1;
+			res[i] = b-a, j = i, r = b<<1;
 		}
 	}
 	return res;
