de643164c8e6cda3fe89c2e3ae9b0fdda878f39a
Andrew He <he.andrew.mail@gmail.com>
2020-03-19 17:59:06 -0700

    Add odd-length-only manacher, if you want that for whatever reason

--- src/manacher.hpp
+++ src/manacher.hpp
@@ -32,3 +32,31 @@ template <typename V> std::vector<int> manacher(const V& S) {
 	}
 	return res;
 }
+
+/**
+ * manacher_odd(S): return the maximum palindromic substring of S centered at each point
+ *
+ * Input: string (or vector) of length N (no restrictions on character-set)
+ * Output: vector res of length N
+ *   For any 0 <= i < N:
+ *   * the half-open substring S[i-res[i], i+res[i]] is a palindrome of length 2*res[i]+1
+ */
+template <typename V> std::vector<int> manacher_odd(const V& S) {
+	int N = int(S.size());
+	std::vector<int> res(N);
+	for (int i = 1, j = -1, r = 0; i < N; i++, j--) {
+		if (i > r) {
+			r = i, res[i] = 0;
+		} else {
+			res[i] = res[j];
+		}
+		if (i+res[i] >= r) {
+			int b = r, a = 2*i-r;
+			while (a-1 >= 0 && b+1 < N && S[a-1] == S[b+1]) {
+				a--, b++;
+			}
+			res[i] = b-i, j = i, r = b;
+		}
+	}
+	return res;
+}
