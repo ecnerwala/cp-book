9ac0f7268200dd05193015a65a60c133aec65fbc
Andrew He <he.andrew.mail@gmail.com>
2020-04-05 17:02:36 -0700

    Add order statistic tree

---
+++ src/order_statistic.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include<bits/extc++.h>
+
+template <typename K, typename V, typename Comp = std::less<K>>
+using order_statistic_map = __gnu_pbds::tree<
+	K, V, Comp,
+	__gnu_pbds::rb_tree_tag,
+	__gnu_pbds::tree_order_statistics_node_update
+>;
+
+template <typename K, typename Comp = std::less<K>>
+using order_statistic_set = order_statistic_map<K, __gnu_pbds::null_type, Comp>;
+
+// Supports
+//  auto iterator = order_statistic_set().find_by_order(int); // (0-indexed)
+//  int num_strictly_smaller = order_statistic_set().order_of_key(key);
