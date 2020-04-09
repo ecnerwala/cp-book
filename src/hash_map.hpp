c7b33a0f459f6cf4c7b6d7aad5bfbdcf5ba65fdf
Andrew He <he.andrew.mail@gmail.com>
2020-04-08 19:09:14 -0700

    Add properly randomized hash-map

---
+++ src/hash_map.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include<bits/stdc++.h>
+#include<bits/extc++.h>
+
+struct splitmix64_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = std::chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+template <typename K, typename V, typename Hash = splitmix64_hash>
+using hash_map = __gnu_pbds::gp_hash_table<K, V, Hash>;
+
+template <typename K, typename Hash = splitmix64_hash>
+using hash_set = hash_map<K, __gnu_pbds::null_type, Hash>;
