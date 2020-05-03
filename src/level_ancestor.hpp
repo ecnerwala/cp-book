e39f91f53e53f0ce9cdf1dac1c657f9734c19e34
Andrew He <he.andrew.mail@gmail.com>
2020-05-03 14:10:22 -0700

    Add level_ancestor/lca code

---
+++ src/level_ancestor.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <vector>
+#include <cassert>
+
+#include "yc.hpp"
+
+namespace ecnerwala {
+
+using std::swap;
+
+struct level_ancestor {
+	int N;
+	std::vector<int> preorder;
+	std::vector<int> idx;
+	std::vector<std::pair<int, int>> heavyPar; // heavy parent, distance
+	level_ancestor() : N(0) {}
+
+	level_ancestor(const std::vector<int>& par) : N(int(par.size())), preorder(N), idx(N), heavyPar(N) {
+		std::vector<std::vector<int>> ch(N);
+		for (int i = 0; i < N; i++) {
+			if (par[i] != -1) ch[par[i]].push_back(i);
+		}
+		std::vector<int> sz(N);
+		int nxt_idx = 0;
+		for (int i = 0; i < N; i++) {
+			if (par[i] == -1) {
+				std::y_combinator([&](auto self, int cur) -> void {
+					sz[cur] = 1;
+					for (int nxt : ch[cur]) {
+						self(nxt);
+						sz[cur] += sz[nxt];
+					}
+					if (!ch[cur].empty()) {
+						auto mit = max_element(ch[cur].begin(), ch[cur].end(), [&](int a, int b) { return sz[a] < sz[b]; });
+						swap(*ch[cur].begin(), *mit);
+					}
+				})(i);
+				std::y_combinator([&](auto self, int cur, int isRoot = true) -> void {
+					preorder[idx[cur] = nxt_idx++] = cur;
+					if (isRoot) {
+						heavyPar[idx[cur]] = {par[cur] == -1 ? -1 : idx[par[cur]], 1};
+					} else {
+						assert(idx[par[cur]] == idx[cur]-1);
+						heavyPar[idx[cur]] = heavyPar[idx[cur]-1];
+						heavyPar[idx[cur]].second++;
+					}
+					bool chRoot = false;
+					for (int nxt : ch[cur]) {
+						self(nxt, chRoot);
+						chRoot = true;
+					}
+				})(i);
+			}
+		}
+	}
+
+	int get_ancestor(int a, int k) const {
+		assert(k >= 0);
+		a = idx[a];
+		while (a != -1 && k) {
+			if (k >= heavyPar[a].second) {
+				k -= heavyPar[a].second;
+				assert(heavyPar[a].first <= a - heavyPar[a].second);
+				a = heavyPar[a].first;
+			} else {
+				a -= k;
+				k = 0;
+			}
+		}
+		if (a == -1) return -1;
+		else return preorder[a];
+	}
+
+	int lca(int a, int b) const {
+		a = idx[a], b = idx[b];
+		while (true) {
+			if (a > b) swap(a, b);
+			assert(a <= b);
+			if (a > b - heavyPar[b].second) {
+				return preorder[a];
+			}
+			b = heavyPar[b].first;
+			if (b == -1) return -1;
+		}
+	}
+};
+
+} // namespace ecnerwala
