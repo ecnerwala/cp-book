1ee6016895615b2f41f99c7e471262a9d09e2ad4
Andrew He <he.andrew.mail@gmail.com>
2020-05-05 22:11:13 -0700

    [tensor] Add tensor class which provides a rectangular non-resizable nD array

---
+++ src/tensor.hpp
@@ -0,0 +1,151 @@
+#pragma once
+
+#include <array>
+
+template <typename T, int NDIMS> struct tensor_view {
+	static_assert(NDIMS >= 0);
+
+protected:
+	std::array<int, NDIMS> shape;
+	std::array<int, NDIMS> strides;
+	T* data;
+
+	tensor_view(std::array<int, NDIMS> shape_, std::array<int, NDIMS> strides_, T* data_) : shape(shape_), strides(strides_), data(data_) {}
+
+public:
+	tensor_view() : shape{0}, strides{0}, data(nullptr) {}
+
+protected:
+	int flatten_index(std::array<int, NDIMS> idx) const {
+		int res = 0;
+		for (int i = 0; i < NDIMS; i++) { res += idx[i] * strides[i]; }
+		return res;
+	}
+	int flatten_index_checked(std::array<int, NDIMS> idx) const {
+		int res = 0;
+		for (int i = 0; i < NDIMS; i++) {
+			assert(0 <= idx[i] && idx[i] < shape[i]);
+			res += idx[i] * strides[i];
+		}
+		return res;
+	}
+
+public:
+	T& operator[] (std::array<int, NDIMS> idx) const {
+		return data[flatten_index(idx)];
+	}
+	T& at(std::array<int, NDIMS> idx) const {
+		return data[flatten_index_checked(idx)];
+	}
+
+	template <int D = NDIMS>
+	std::enable_if_t<(0 < D), tensor_view<T, NDIMS-1>> operator[] (int idx) const {
+		std::array<int, NDIMS-1> nshape; std::copy(shape.begin()+1, shape.end(), nshape.begin());
+		std::array<int, NDIMS-1> nstrides; std::copy(strides.begin()+1, strides.end(), nstrides.begin());
+		T* ndata = data + (strides[0] * idx);
+		return tensor_view<T, NDIMS-1>(nshape, nstrides, ndata);
+	}
+	template <int D = NDIMS>
+	std::enable_if_t<(0 < D), tensor_view<T, NDIMS-1>> at(int idx) const {
+		assert(0 <= idx && idx < shape[0]);
+		return operator[](idx);
+	}
+
+	template <int D = NDIMS>
+	std::enable_if_t<(0 == D), T&> operator * () const {
+		return *data;
+	}
+
+	template <typename U, int D> friend struct tensor_view;
+	template <typename U, int D> friend struct tensor;
+};
+
+template <typename T, int NDIMS> struct tensor {
+protected:
+	std::array<int, NDIMS> shape;
+	std::array<int, NDIMS> strides;
+	int len;
+	T* data;
+
+public:
+	tensor() : shape{0}, strides{0}, len(0) {}
+
+	explicit tensor(std::array<int, NDIMS> shape_, const T& t = T()) {
+		shape = shape_;
+		strides[NDIMS-1] = 1;
+		for (int i = NDIMS-1; i > 0; i--) {
+			strides[i-1] = strides[i] * shape[i];
+		}
+		len = strides[0] * shape[0];
+		data = new T[len]{t};
+	}
+
+	tensor(const tensor& o) : shape(o.shape), strides(o.strides), len(o.len), data(new T[len]) {
+		for (int i = 0; i < len; i++) {
+			data[i] = o.data[i];
+		}
+	}
+
+	tensor& operator=(tensor&& o) noexcept {
+		using std::swap;
+		swap(shape, o.shape);
+		swap(strides, o.strides);
+		swap(len, o.len);
+		swap(data, o.data);
+	}
+	tensor(tensor&& o) : tensor() {
+		*this = std::move(o);
+	}
+	tensor& operator=(const tensor& o) {
+		return *this = tensor(o);
+	}
+	~tensor() { delete[] data; }
+
+	using view_t = tensor_view<T, NDIMS>;
+	view_t view() {
+		return tensor_view<T, NDIMS>(shape, strides, data);
+	}
+	operator view_t() {
+		return view();
+	}
+
+	using const_view_t = tensor_view<const T, NDIMS>;
+	const_view_t view() const {
+		return tensor_view<const T, NDIMS>(shape, strides, data);
+	}
+	operator const_view_t() const {
+		return view();
+	}
+
+	T& operator[] (std::array<int, NDIMS> idx) { return view()[idx]; }
+	T& at(std::array<int, NDIMS> idx) { return view().at(idx); }
+	const T& operator[] (std::array<int, NDIMS> idx) const { return view()[idx]; }
+	const T& at(std::array<int, NDIMS> idx) const { return view().at(idx); }
+
+	template <int D = NDIMS>
+	std::enable_if_t<(0 < D), tensor_view<T, NDIMS-1>> operator[] (int idx) {
+		return view()[idx];
+	}
+	template <int D = NDIMS>
+	std::enable_if_t<(0 < D), tensor_view<T, NDIMS-1>> at(int idx) {
+		return view().at(idx);
+	}
+
+	template <int D = NDIMS>
+	std::enable_if_t<(0 < D), tensor_view<const T, NDIMS-1>> operator[] (int idx) const {
+		return view()[idx];
+	}
+	template <int D = NDIMS>
+	std::enable_if_t<(0 < D), tensor_view<const T, NDIMS-1>> at(int idx) const {
+		return view().at(idx);
+	}
+
+	template <int D = NDIMS>
+	std::enable_if_t<(0 == D), T&> operator * () {
+		return *view();
+	}
+	template <int D = NDIMS>
+	std::enable_if_t<(0 == D), const T&> operator * () const {
+		return *view();
+	}
+};
---
+++ src/tensor.test.cpp
@@ -0,0 +1,47 @@
+#include "catch.hpp"
+
+#include "tensor.hpp"
+
+#include <string>
+
+TEST_CASE("Tensor", "[tensor]") {
+	using ten = tensor<std::string, 2>;
+	ten a({2, 3});
+	a[{0,0}] = "0";
+	a[{0,1}] = "1";
+	a[{0,2}] = "2";
+	a[{1,0}] = "3";
+	a[{1,1}] = "4";
+	a[{1,2}] = "5";
+
+	const ten const_a = a;
+	ten b = a;
+	REQUIRE(b[{0,0}] == "0");
+	REQUIRE(b[{0,1}] == "1");
+	REQUIRE(b[{0,2}] == "2");
+	REQUIRE(b[{1,0}] == "3");
+	REQUIRE(b[{1,1}] == "4");
+	REQUIRE(b[{1,2}] == "5");
+
+	// Bounds checked
+	REQUIRE(b.at({0,0}) == "0");
+	REQUIRE(b.at({0,1}) == "1");
+	REQUIRE(b.at({0,2}) == "2");
+	REQUIRE(b.at({1,0}) == "3");
+	REQUIRE(b.at({1,1}) == "4");
+	REQUIRE(b.at({1,2}) == "5");
+
+	REQUIRE(*b[0][0] == "0");
+	REQUIRE(*b[0][1] == "1");
+	REQUIRE(*b[0][2] == "2");
+	REQUIRE(*b[1][0] == "3");
+	REQUIRE(*b[1][1] == "4");
+	REQUIRE(*b[1][2] == "5");
+
+	REQUIRE(*const_a[0][0] == "0");
+	REQUIRE(*const_a[0][1] == "1");
+	REQUIRE(*const_a[0][2] == "2");
+	REQUIRE(*const_a[1][0] == "3");
+	REQUIRE(*const_a[1][1] == "4");
+	REQUIRE(*const_a[1][2] == "5");
+}
