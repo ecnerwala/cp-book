127ca30879bf6c44d7a3ab5cf1afaf8e3a619221
Andrew He <he.andrew.mail@gmail.com>
2020-11-17 22:09:43 -0800

    Turn the binary-indexed-tree into an iterator

--- src/bit.hpp
+++ src/bit.hpp
@@ -5,11 +5,11 @@
 
 /** Binary-indexed tree
  *
- *  A binary indexed tree with N nodes of type T implicitly provides the
+ *  A binary indexed tree with N nodes of type T provides the
  *  following two functions for 0 <= i <= N:
  *
- *      prefix(int i) -> list<T&>
- *      suffix(int i) -> list<T&>
+ *      prefix(int i) -> prefix_iterator<T>
+ *      suffix(int i) -> suffix_iterator<T>
  *
  *  such that size(suffix(i) intersect prefix(j)) = (1 if i < j else 0).
  *  Furthermore, the resulting lists always have size at most log_2(N).
@@ -25,7 +25,7 @@
  *   * For prefix update/query, no change.
  *   * For suffix update/query, use for_prefix(point + 1); 1-index the data.
  */
-template <typename T> struct binary_indexed_tree {
+template <typename T> class binary_indexed_tree {
 private:
 	std::vector<T> dat;
 public:
@@ -33,31 +33,119 @@ public:
 	explicit binary_indexed_tree(size_t N) : dat(N) {}
 	binary_indexed_tree(size_t N, const T& t) : dat(N, t) {}
 
-	template <typename F> void for_prefix(int i, F f) const {
-		assert(0 <= i && i <= int(dat.size()));
-		for (int a = i; a; a &= a-1) {
-			f(dat[a-1]);
+	size_t size() const { return dat.size(); }
+	const std::vector<T>& data() const { return dat; }
+	std::vector<T>& data() { return dat; }
+
+private:
+	template <typename I, typename S = I> struct iterator_range {
+	private:
+		I begin_;
+		S end_;
+	public:
+		iterator_range() : begin_(), end_() {}
+		iterator_range(const I& begin__, const S& end__) : begin_(begin__), end_(end__) {}
+		iterator_range(I&& begin__, S&& end__) : begin_(begin__), end_(end__) {}
+		I begin() const { return begin_; }
+		S end() const { return end_; }
+	};
+
+public:
+	class const_suffix_iterator {
+	private:
+		const T* dat;
+		int a;
+		const_suffix_iterator(const T* dat_, int a_) : dat(dat_), a(a_) {}
+		friend class binary_indexed_tree;
+	public:
+		friend bool operator != (const const_suffix_iterator& i, const const_suffix_iterator& j) {
+			assert(j.dat == nullptr);
+			return i.a < j.a;
 		}
+		const_suffix_iterator& operator ++ () {
+			a |= a+1;
+			return *this;
+		}
+		const T& operator * () const {
+			return dat[a];
+		}
+	};
+	using const_suffix_range = iterator_range<const_suffix_iterator>;
+	const_suffix_range suffix(int a) const {
+		assert(0 <= a && a <= int(dat.size()));
+		return const_suffix_range{const_suffix_iterator{dat.data(), a}, const_suffix_iterator{nullptr, int(dat.size())}};
 	}
 
-	template <typename F> void for_prefix(int i, F f) {
-		assert(0 <= i && i <= int(dat.size()));
-		for (int a = i; a; a &= a-1) {
-			f(dat[a-1]);
+	class suffix_iterator {
+	private:
+		T* dat;
+		int a;
+		suffix_iterator(T* dat_, int a_) : dat(dat_), a(a_) {}
+		friend class binary_indexed_tree;
+	public:
+		friend bool operator != (const suffix_iterator& i, const suffix_iterator& j) {
+			assert(j.dat == nullptr);
+			return i.a < j.a;
 		}
+		suffix_iterator& operator ++ () {
+			a |= a+1;
+			return *this;
+		}
+		T& operator * () const {
+			return dat[a];
+		}
+	};
+	using suffix_range = iterator_range<suffix_iterator>;
+	suffix_range suffix(int a) {
+		assert(0 <= a && a <= int(dat.size()));
+		return suffix_range{suffix_iterator{dat.data(), a}, suffix_iterator{nullptr, int(dat.size())}};
 	}
 
-	template <typename F> void for_suffix(int i, F f) const {
-		assert(0 <= i && i <= int(dat.size()));
-		for (int a = i; a < int(dat.size()); a |= a+1) {
-			f(dat[a]);
+	class const_prefix_iterator {
+	private:
+		const T* dat;
+		int a;
+		const_prefix_iterator(const T* dat_, int a_) : dat(dat_), a(a_) {}
+		friend class binary_indexed_tree;
+	public:
+		friend bool operator != (const const_prefix_iterator& i, const const_prefix_iterator& j) {
+			assert(j.dat == nullptr);
+			return i.a > 0;
 		}
+		const_prefix_iterator& operator ++ () {
+			a &= a-1;
+			return *this;
+		}
+		const T& operator * () const {
+			return dat[a-1];
+		}
+	};
+	using const_prefix_range = iterator_range<const_prefix_iterator>;
+	const_prefix_range prefix(int a) const {
+		return const_prefix_range{const_prefix_iterator{dat.data(), a}, const_prefix_iterator{nullptr, 0}};
 	}
 
-	template <typename F> void for_suffix(int i, F f) {
-		assert(0 <= i && i <= int(dat.size()));
-		for (int a = i; a < int(dat.size()); a |= a+1) {
-			f(dat[a]);
+	class prefix_iterator {
+	private:
+		T* dat;
+		int a;
+		prefix_iterator(T* dat_, int a_) : dat(dat_), a(a_) {}
+		friend class binary_indexed_tree;
+	public:
+		friend bool operator != (const prefix_iterator& i, const prefix_iterator& j) {
+			assert(j.dat == nullptr);
+			return i.a > 0;
 		}
+		prefix_iterator& operator ++ () {
+			a &= a-1;
+			return *this;
+		}
+		T& operator * () const {
+			return dat[a-1];
+		}
+	};
+	using prefix_range = iterator_range<prefix_iterator>;
+	prefix_range prefix(int a) {
+		return prefix_range{prefix_iterator{dat.data(), a}, prefix_iterator{nullptr, 0}};
 	}
 };
