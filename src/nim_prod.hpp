e63de28be2753424f9a8b45b68bb07bc1c8af27f
Andrew He <he.andrew.mail@gmail.com>
2021-02-27 11:43:12 -0800

    [nim_prod] Update to use a struct

--- src/nim_prod.hpp
+++ src/nim_prod.hpp
@@ -3,32 +3,31 @@
 #include <utility>
 #include <cstdint>
 
-using ull = uint64_t;
-ull _nimProd2[64][64];
-ull nimProd2(int i, int j) {
-	if (_nimProd2[i][j]) return _nimProd2[i][j];
-	if ((i & j) == 0) return _nimProd2[i][j] = 1ull << (i|j);
-	int a = (i&j) & -(i&j);
-	return _nimProd2[i][j] = nimProd2(i ^ a, j) ^ nimProd2((i ^ a) | (a-1), (j ^ a) | (i & (a-1)));
-}
-void allNimProd2() {
-	for (int i = 0; i < 64; i++) {
-		for (int j = 0; j < 64; j++) {
-			if ((i & j) == 0) {
-				_nimProd2[i][j] = 1ull << (i|j);
-			} else {
-				int a = (i&j) & -(i&j);
-				_nimProd2[i][j] = _nimProd2[i ^ a][j] ^ _nimProd2[(i ^ a) | (a-1)][(j ^ a) | (i & (a-1))];
+// Usage:
+//   constexpr nim_prod_t nimProd;
+// C++20:
+//   constinit nim_prod_t nimProd;
+struct nim_prod_t {
+	uint64_t bit_prod[64][64]{};
+	constexpr nim_prod_t() {
+		for (int i = 0; i < 64; i++) {
+			for (int j = 0; j < 64; j++) {
+				if ((i & j) == 0) {
+					bit_prod[i][j] = uint64_t(1) << (i|j);
+				} else {
+					int a = (i&j) & -(i&j);
+					bit_prod[i][j] = bit_prod[i ^ a][j] ^ bit_prod[(i ^ a) | (a-1)][(j ^ a) | (i & (a-1))];
+				}
 			}
 		}
 	}
-}
-ull nimProd(ull x, ull y) {
-	ull res = 0;
-	for (int i = 0; (x >> i) && i < 64; i++)
-		if ((x >> i) & 1)
-			for (int j = 0; (y >> j) && j < 64; j++)
-				if ((y >> j) & 1)
-					res ^= nimProd2(i, j);
-	return res;
-}
+	constexpr uint64_t operator () (uint64_t x, uint64_t y) const {
+		uint64_t res = 0;
+		for (int i = 0; i < 64 && (x >> i); i++)
+			if ((x >> i) & 1)
+				for (int j = 0; j < 64 && (y >> j); j++)
+					if ((y >> j) & 1)
+						res ^= bit_prod[i][j];
+		return res;
+	}
+};
