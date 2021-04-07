910791691bf9e07ed90beb3c75af8449440980b8
Andrew He <he.andrew.mail@gmail.com>
2021-04-06 23:18:48 -0700

    [geometry] Improvements to point

--- src/geometry/point.hpp
+++ src/geometry/point.hpp
@@ -19,6 +19,9 @@ public:
 	friend bool operator == (const Point& a, const Point& b) { return a.x == b.x && a.y == b.y; }
 	friend bool operator != (const Point& a, const Point& b) { return !(a==b); }
 
+	Point operator + () const { return Point(+x, +y); }
+	Point operator - () const { return Point(-x, -y); }
+
 	Point& operator += (const Point& p) { x += p.x, y += p.y; return *this; }
 	Point& operator -= (const Point& p) { x -= p.x, y -= p.y; return *this; }
 	Point& operator *= (const T& t) { x *= t, y *= t; return *this; }
@@ -30,19 +33,40 @@ public:
 	friend Point operator * (const T& t ,const Point& a) { return Point(t*a.x, t*a.y); }
 	friend Point operator / (const Point& a, const T& t) { return Point(a.x/t, a.y/t); }
 
-	friend T norm(const Point& a) { return a.x * a.x + a.y * a.y; }
-	friend auto abs(const Point& p) { return std::hypot(p.x, p.y); }
-	friend T unit(const Point& a) { if (a == Point()) return a; return a / abs(a); }
+	T dist2() const { return x * x + y * y; }
+	auto dist() const { return std::sqrt(dist2()); }
+	Point unit() const { return *this / this->dist(); }
+	auto angle() const { return std::atan2(y, x); }
 
-	friend T int_norm(const Point& a) { return __gcd(a.x,a.y); }
-	friend T int_unit(const Point& a) { if (a == Point()) return a; return a / int_norm(a); }
+	T int_norm() const { return __gcd(x,y); }
+	Point int_unit() const { if (!x && !y) return *this; return *this / this->int_norm(); }
 
-	friend T cross(const Point& a, const Point& b) { return a.x * b.y - a.y * b.x; }
+	// Convenient free-functions, mostly for generic interop
+	friend auto norm(const Point& a) { return a.dist2(); }
+	friend auto abs(const Point& a) { return a.dist(); }
+	friend auto unit(const Point& a) { return a.unit(); }
+	friend auto arg(const Point& a) { return a.angle(); }
+	friend auto int_norm(const Point& a) { return a.int_norm(); }
+	friend auto int_unit(const Point& a) { return a.int_unit(); }
+
+	Point perp_cw() const { return Point(y, -x); }
+	Point perp_ccw() const { return Point(-y, x); }
+
 	friend T dot(const Point& a, const Point& b) { return a.x * b.x + a.y * b.y; }
-	friend T area(const Point& a, const Point& b, const Point& c) { return cross(b-a, c-a); }
+	friend T cross(const Point& a, const Point& b) { return a.x * b.y - a.y * b.x; }
+	friend T cross3(const Point& a, const Point& b, const Point& c) { return cross(b-a, c-a); }
+
+	// Complex numbers and rotation
+	friend Point conj(const Point& a) { return Point(a.x, -a.y); }
 
 	// Returns conj(a) * b
-	friend Point rotation(const Point& a, const Point& b) { return Point(dot(a, b), cross(a, b)); }
+	friend Point dot_cross(const Point& a, const Point& b) { return Point(dot(a, b), cross(a, b)); }
+	friend Point cmul(const Point& a, const Point& b) { return dot_cross(conj(a), b); }
+	friend Point cdiv(const Point& a, const Point& b) { return dot_cross(b, a) / b.norm(); }
+
+	// Must be a unit vector; otherwise multiplies the result by abs(u)
+	Point rotate(const Point& u) const { return dot_cross(conj(u), *this); }
+	Point unrotate(const Point& u) const { return dot_cross(u, *this); }
 
 	friend bool same_dir(const Point& a, const Point& b) { return cross(a,b) == 0 && dot(a,b) > 0; }
 
