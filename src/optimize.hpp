11f9f7c36097a18a0289c3cbdc158408c874927d
Andrew He <he.andrew.mail@gmail.com>
2021-08-03 19:48:34 -0700

    [optimize] Add documentation of optimization pragmas

---
+++ src/optimize.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,popcnt,abm,mmx") // Safe for yandex
+
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma") // Requires AVX2
