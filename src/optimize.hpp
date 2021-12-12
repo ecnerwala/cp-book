5ddbb827dd71ad072d7243783d816333f4448f6a
Andrew He <he.andrew.mail@gmail.com>
2021-12-11 21:44:16 -0800

    [optimize] Update a little

--- src/optimize.hpp
+++ src/optimize.hpp
@@ -3,4 +3,6 @@
 #pragma GCC optimize("Ofast")
 #pragma GCC target("sse,sse2,sse3,ssse3,popcnt,abm,mmx") // Safe for yandex
 
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma") // Requires AVX2
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,bmi,bmi2,mmx,avx,avx2,fma") // Requires AVX2
+
+// See https://codeforces.com/blog/entry/96344
