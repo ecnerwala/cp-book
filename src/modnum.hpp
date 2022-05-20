cf2893d1ff4418e735870ee904be5ce269f21c5b
Andrew He <ecnerwala@users.noreply.github.com>
2022-05-20 01:20:22 -0400

    Add dynamic modnum

--- src/modnum.hpp
+++ src/modnum.hpp
@@ -29,17 +29,15 @@ template <int MOD_> struct modnum {
 	static_assert(MOD_ > 0, "MOD must be positive");
 
 private:
-	using ll = long long;
-
 	int v;
 
 public:
 
 	modnum() : v(0) {}
-	modnum(ll v_) : v(int(v_ % MOD)) { if (v < 0) v += MOD; }
+	modnum(int64_t v_) : v(int(v_ % MOD)) { if (v < 0) v += MOD; }
 	explicit operator int() const { return v; }
 	friend std::ostream& operator << (std::ostream& out, const modnum& n) { return out << int(n); }
-	friend std::istream& operator >> (std::istream& in, modnum& n) { ll v_; in >> v_; n = modnum(v_); return in; }
+	friend std::istream& operator >> (std::istream& in, modnum& n) { int64_t v_; in >> v_; n = modnum(v_); return in; }
 
 	friend bool operator == (const modnum& a, const modnum& b) { return a.v == b.v; }
 	friend bool operator != (const modnum& a, const modnum& b) { return a.v != b.v; }
@@ -85,7 +83,7 @@ public:
 		return *this;
 	}
 	modnum& operator *= (const modnum& o) {
-		v = int(ll(v) * ll(o.v) % MOD);
+		v = int(int64_t(v) * int64_t(o.v) % MOD);
 		return *this;
 	}
 	modnum& operator /= (const modnum& o) {
@@ -168,4 +166,130 @@ template <typename U, typename V> struct pairnum {
 	friend pairnum operator - (const pairnum& a, const pairnum& b) { return pairnum(a) -= b; }
 	friend pairnum operator * (const pairnum& a, const pairnum& b) { return pairnum(a) *= b; }
 	friend pairnum operator / (const pairnum& a, const pairnum& b) { return pairnum(a) /= b; }
+};
+
+template <typename tag> struct dynamic_modnum {
+private:
+#if __cpp_inline_variables >= 201606
+	// C++17 and up
+	inline static int MOD_ = 0;
+	inline static uint64_t BARRETT_M = 0;
+#else
+	// NB: these must be initialized out of the class by hand:
+	//   static int dynamic_modnum<tag>::MOD = 0;
+	//   static int dynamic_modnum<tag>::BARRETT_M = 0;
+	static int MOD_;
+	static uint64_t BARRETT_M;
+#endif
+
+public:
+	// Make only the const-reference public, to force the use of set_mod
+	static constexpr int const& MOD = MOD_;
+
+	// Barret reduction taken from KACTL:
+	/**
+	 * Author: Simon Lindholm
+	 * Date: 2020-05-30
+	 * License: CC0
+	 * Source: https://en.wikipedia.org/wiki/Barrett_reduction
+	 * Description: Compute $a \% b$ about 5 times faster than usual, where $b$ is constant but not known at compile time.
+	 * Returns a value congruent to $a \pmod b$ in the range $[0, 2b)$.
+	 * Status: proven correct, stress-tested
+	 * Measured as having 4 times lower latency, and 8 times higher throughput, see stress-test.
+	 * Details:
+	 * More precisely, it can be proven that the result equals 0 only if $a = 0$,
+	 * and otherwise lies in $[1, (1 + a/2^64) * b)$.
+	 */
+	static void set_mod(int mod) {
+		assert(mod > 0);
+		MOD_ = mod;
+		BARRETT_M = (uint64_t(-1) / MOD);
+	}
+	static uint32_t barrett_reduce_partial(uint64_t a) {
+		return uint32_t(a - uint64_t((__uint128_t(BARRETT_M) * a) >> 64) * MOD);
+	}
+	static int barrett_reduce(uint64_t a) {
+		int32_t res = int32_t(barrett_reduce_partial(a) - MOD);
+		return (res < 0) ? res + MOD : res;
+	}
+
+	struct mod_reader {
+		friend std::istream& operator >> (std::istream& i, mod_reader) {
+			int mod; i >> mod;
+			dynamic_modnum::set_mod(mod);
+			return i;
+		}
+	};
+	static mod_reader MOD_READER() {
+		return mod_reader();
+	}
+
+private:
+	int v;
+
+public:
+
+	dynamic_modnum() : v(0) {}
+	dynamic_modnum(int64_t v_) : v(int(v_ % MOD)) { if (v < 0) v += MOD; }
+	explicit operator int() const { return v; }
+	friend std::ostream& operator << (std::ostream& out, const dynamic_modnum& n) { return out << int(n); }
+	friend std::istream& operator >> (std::istream& in, dynamic_modnum& n) { int64_t v_; in >> v_; n = dynamic_modnum(v_); return in; }
+
+	friend bool operator == (const dynamic_modnum& a, const dynamic_modnum& b) { return a.v == b.v; }
+	friend bool operator != (const dynamic_modnum& a, const dynamic_modnum& b) { return a.v != b.v; }
+
+	dynamic_modnum inv() const {
+		dynamic_modnum res;
+		res.v = mod_inv_in_range(v, MOD);
+		return res;
+	}
+	friend dynamic_modnum inv(const dynamic_modnum& m) { return m.inv(); }
+	dynamic_modnum neg() const {
+		dynamic_modnum res;
+		res.v = v ? MOD-v : 0;
+		return res;
+	}
+	friend dynamic_modnum neg(const dynamic_modnum& m) { return m.neg(); }
+
+	dynamic_modnum operator- () const {
+		return neg();
+	}
+	dynamic_modnum operator+ () const {
+		return dynamic_modnum(*this);
+	}
+
+	dynamic_modnum& operator ++ () {
+		v ++;
+		if (v == MOD) v = 0;
+		return *this;
+	}
+	dynamic_modnum& operator -- () {
+		if (v == 0) v = MOD;
+		v --;
+		return *this;
+	}
+	dynamic_modnum& operator += (const dynamic_modnum& o) {
+		v -= MOD-o.v;
+		v = (v < 0) ? v + MOD : v;
+		return *this;
+	}
+	dynamic_modnum& operator -= (const dynamic_modnum& o) {
+		v -= o.v;
+		v = (v < 0) ? v + MOD : v;
+		return *this;
+	}
+	dynamic_modnum& operator *= (const dynamic_modnum& o) {
+		v = barrett_reduce(int64_t(v) * int64_t(o.v));
+		return *this;
+	}
+	dynamic_modnum& operator /= (const dynamic_modnum& o) {
+		return *this *= o.inv();
+	}
+
+	friend dynamic_modnum operator ++ (dynamic_modnum& a, int) { dynamic_modnum r = a; ++a; return r; }
+	friend dynamic_modnum operator -- (dynamic_modnum& a, int) { dynamic_modnum r = a; --a; return r; }
+	friend dynamic_modnum operator + (const dynamic_modnum& a, const dynamic_modnum& b) { return dynamic_modnum(a) += b; }
+	friend dynamic_modnum operator - (const dynamic_modnum& a, const dynamic_modnum& b) { return dynamic_modnum(a) -= b; }
+	friend dynamic_modnum operator * (const dynamic_modnum& a, const dynamic_modnum& b) { return dynamic_modnum(a) *= b; }
+	friend dynamic_modnum operator / (const dynamic_modnum& a, const dynamic_modnum& b) { return dynamic_modnum(a) /= b; }
 };
