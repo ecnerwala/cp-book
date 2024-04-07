6b20ac0f7df563541f201a5c253b2c0b4ab7d054
Andrew He <7088371+ecnerwala@users.noreply.github.com>
2024-04-07 01:56:01 -0400

    [mcmf] Add more stuff to Dinic

--- src/mcmf.hpp
+++ src/mcmf.hpp
@@ -239,7 +239,7 @@ struct MCMF_Dinic {
 	}
 };
 
-template <typename flow_t = int>
+template <typename flow_t = int, typename tot_flow_t = flow_t>
 struct Dinic {
 	int N;
 	std::vector<std::vector<int>> adj;
@@ -254,26 +254,31 @@ struct Dinic {
 	explicit Dinic(int N_) : N(N_), adj(N) {}
 
 	void add_edge(int from, int to, flow_t cap) {
+		return add_bi_edge(from, to, cap, 0);
+	}
+
+	void add_bi_edge(int from, int to, flow_t cap, flow_t rev_cap) {
 		assert(cap >= 0);
+		assert(rev_cap >= 0);
 		int e = int(edges.size());
 		edges.emplace_back(edge_t{to, cap});
-		edges.emplace_back(edge_t{from, 0});
+		edges.emplace_back(edge_t{from, rev_cap});
 		adj[from].push_back(e);
 		adj[to].push_back(e+1);
 	}
 
-	static constexpr flow_t INF_FLOW = std::numeric_limits<flow_t>::max() / 4;
+	static constexpr tot_flow_t INF_FLOW = std::numeric_limits<tot_flow_t>::max() / 4;
 	std::vector<int> buf;
 	std::vector<int> level;
-	flow_t dinic_dfs(int cur, int t, flow_t f) {
+	tot_flow_t dinic_dfs(int cur, int t, tot_flow_t f) {
 		if (cur == t) return f;
-		flow_t cur_f = 0;
+		tot_flow_t cur_f = 0;
 		assert(f > 0);
 		for (; buf[cur] < int(adj[cur].size()); buf[cur]++) {
 			int e = adj[cur][buf[cur]];
 			int nxt = edges[e].dest;
 			if (level[nxt] == level[cur] + 1 && edges[e].cap > 0) {
-				flow_t v = dinic_dfs(nxt, t, std::min(f, edges[e].cap));
+				flow_t v = flow_t(dinic_dfs(nxt, t, std::min<tot_flow_t>(f, edges[e].cap)));
 				edges[e].cap -= v;
 				edges[e^1].cap += v;
 				f -= v;
@@ -283,8 +288,8 @@ struct Dinic {
 		}
 		return cur_f;
 	}
-	flow_t dinic(int s, int t) {
-		flow_t tot_flow = 0;
+	tot_flow_t dinic(int s, int t) {
+		tot_flow_t tot_flow = 0;
 		while (true) {
 			buf.clear();
 			buf.reserve(N);
@@ -307,5 +312,5 @@ struct Dinic {
 		}
 		return tot_flow;
 	}
-	flow_t max_flow(int s, int t) { return dinic(s, t); }
+	tot_flow_t max_flow(int s, int t) { return dinic(s, t); }
 };
