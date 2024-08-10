2a53e2d7d46cab0cee2f23fe0eb11248cb4db196
Andrew He <7088371+ecnerwala@users.noreply.github.com>
2024-08-10 15:40:49 -0700

    [bm] Clean up linearRec

--- src/bm.hpp
+++ src/bm.hpp
@@ -26,28 +26,36 @@ std::vector<num> BerlekampMassey(const std::vector<num>& s) {
 }
 
 template <typename num>
-num linearRec(const std::vector<num>& S, const std::vector<num>& tr, long long k) {
+num linearRec(const std::vector<num>& S, const std::vector<num>& tr, int64_t k) {
 	int n = int(tr.size());
 	assert(S.size() >= tr.size());
 
-	auto combine = [&](std::vector<num> a, std::vector<num> b) {
-		std::vector<num> res(n * 2 + 1);
-		for (int i = 0; i <= n; i++) for (int j = 0; j <= n; j++) res[i + j] += a[i] * b[j];
-		for (int i = 2 * n; i > n; --i) for (int j = 0; j < n; j++)
-			res[i - 1 - j] += res[i] * tr[j];
-		res.resize(n + 1);
+	auto combine = [&](std::vector<num> a, std::vector<num> b, bool e = false) {
+		// multiply a * b * x^e
+		std::vector<num> res(int(a.size()) + int(b.size()));
+		for (int i = 0; i < int(a.size()); i++) {
+			for (int j = 0; j < int(b.size()); j++) {
+				res[i + j + e] += a[i] * b[j];
+			}
+		}
+		for (int i = int(res.size())-1; i >= n; --i) {
+			for (int j = 0; j < n; j++) {
+				res[i - 1 - j] += res[i] * tr[j];
+			}
+		}
+		res.resize(n);
 		return res;
 	};
 
-	std::vector<num> pol(n + 1), e(pol);
-	pol[0] = e[1] = 1;
+	std::vector<num> pol(n);
+	if (n > 0) pol[0] = num(1);
 
-	for (++k; k; k /= 2) {
-		if (k % 2) pol = combine(pol, e);
-		e = combine(e, e);
+	assert(k >= 0);
+	for (int i = 64 - 1 - (k == 0 ? 64 : __builtin_clzll(k)); i >= 0; i--) {
+		pol = combine(pol, pol, (k >> i) & 1);
 	}
 
 	num res = 0;
-	for (int i = 0; i < n; i++) res += pol[i + 1] * S[i];
+	for (int i = 0; i < n; i++) res += pol[i] * S[i];
 	return res;
 }
