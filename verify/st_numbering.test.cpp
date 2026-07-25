0b3065e99607fc5c04c55ddb911bd0c0e0d24b95
Andrew He <7088371+ecnerwala@users.noreply.github.com>
2026-07-25 03:22:03 -0700

    [verify][make_st_dag] Verify st_numbering

---
+++ verify/st_numbering.test.cpp
@@ -0,0 +1,33 @@
+// competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/st_numbering
+
+#include <bits/stdc++.h>
+#include <cassert>
+
+#include "graph/make_st_dag.hpp"
+
+int main() {
+	std::ios_base::sync_with_stdio(false), std::cin.tie(nullptr);
+
+	int N_CASES; std::cin >> N_CASES;
+	while (N_CASES--) {
+		int N, M, S, T; std::cin >> N >> M >> S >> T;
+		std::vector<std::vector<int>> adj(N);
+		for (int e = 0; e < M; e++) {
+			int u, v; std::cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+
+		auto res = make_st_dag(adj, S, T);
+		if (int(res.size()) == N) {
+			std::cout << "Yes" << '\n';
+			for (int i = 0; i < N; i++) {
+				std::cout << res[i] << " \n"[i+1==N];
+			}
+		} else {
+			std::cout << "No" << '\n';
+		}
+	}
+
+	return 0;
+}
