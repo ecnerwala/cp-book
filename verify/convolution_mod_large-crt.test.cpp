11e58b03dee930b04f816f96a5f7051486039894
Andrew He <7088371+ecnerwala@users.noreply.github.com>
2026-07-25 04:34:04 -0700

    [verify][fft] Add large convolution (2^24) via CRT with big primes

---
+++ verify/convolution_mod_large-crt.test.cpp
@@ -0,0 +1,24 @@
+// competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/convolution_mod_large
+
+#include <bits/stdc++.h>
+#include <cassert>
+
+#include "fft.hpp"
+
+int main() {
+	std::ios_base::sync_with_stdio(false), std::cin.tie(nullptr);
+
+	using num = modnum<998244353>;
+	using E = ecnerwala::fft::crt_engine<num>;
+	using pse = ecnerwala::power_series_exact<E>;
+
+	int N, M; std::cin >> N >> M;
+	pse A(N); for (auto& x : A) std::cin >> x;
+	pse B(M); for (auto& x : B) std::cin >> x;
+	pse C = A * B;
+	for (int i = 0; i < N+M-1; i++) {
+		std::cout << C[i] << " \n"[i+1==N+M-1];
+	}
+
+	return 0;
+}
