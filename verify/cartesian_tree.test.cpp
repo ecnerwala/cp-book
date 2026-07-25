cc52e2227b6401752681465cb6e9c58dd06ff2b3
Andrew He <7088371+ecnerwala@users.noreply.github.com>
2026-07-25 03:53:07 -0700

    [verify][cartesian_tree] Fix

--- verify/cartesian_tree.test.cpp
+++ verify/cartesian_tree.test.cpp
@@ -14,7 +14,12 @@ int main() {
 	auto ct = CartesianTree::build_min_tree(A);
 	for (int i = 0; i < N; i++) {
 		int p = ct[2*i+1].p;
-		if (p == -1) p = i;
+		if (p == -1) {
+			p = i;
+		} else {
+			assert(p & 1);
+			p /= 2;
+		}
 		std::cout << p << " \n"[i+1==N];
 	}
 
