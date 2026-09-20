74bc59585925bc3d3d06d44dd6132f25248ee352
Andrew He <7088371+ecnerwala@users.noreply.github.com>
2026-09-20 00:37:19 -0700

    [spqr] Make the dfs non-recursive

--- src/graph/spqr_tree.hpp
+++ src/graph/spqr_tree.hpp
@@ -333,175 +333,228 @@ struct spqr_tree {
 				return res;
 			};
 
+			struct dfs_stack_t {
+				bool has_return_edge;
+				int ch_idx;
+				int orig_tstack;
+			};
+			std::vector<dfs_stack_t> stk; stk.reserve(NV);
 			for (auto rt : roots) {
-				[&](this auto&& self, int cur, int cur_depth) -> void {
+				auto push_vert = [&](int cur) -> void {
+					stk.push_back({false, outedges.bounds[cur], -1});
+					int cur_depth = int(stk.size()) - 1;
 					stack_verts[cur_depth] = cur;
-					bool has_return_edge = false;
-
-					for (auto [_, nxt, e, key] : outedges[cur]) {
-						int lowval = key / 3 - 2; if (lowval < 0) lowval = cur_depth + ~lowval;
-						int kind = key % 3;
-						bool is_tree = kind != 1;
-						bool is_type_1 = kind <= 1;
-
-						// edge_dir convention: false is forwards, true is backwards.
-						// That means that cur is on the edge_dir side and nxt is on the !edge_dir side.
-						bool edge_dir = (lowval >= cur_depth ? false : !stack_dir[lowval]);
-						stack_dir[cur_depth] = edge_dir;
-
-						int orig_tstack = int(tstack.size());
-						if (is_tree) {
-							first_occurrence[cur_depth] = NE;
-							self(nxt, cur_depth + 1);
-						}
-
-						if (lowval >= cur_depth) {
-							item_vs[edge_item(e)] = {cur, -1};
-							tot_blocks++;
-							if (is_tree) {
-								// Bridges and components
-								if (lowval == cur_depth + 1) {
-									// tstack.back() is currently just smuggling out the child vertex, prepend the bridge component
-									// This is just a shortcut for allocating a full I-type tstack
-									int item = alloc_item(node_type::I);
-									item_vs[item] = make_vs(nxt, cur_depth);
-									item_ch[edge_item(e)] = concat(unit_list(item), pop_tstack().spans[1]);
-								} else {
-									// tstack.end()[-2] is the vertex and tstack.end()[-1] is the backedge
-									auto backedge = pop_tstack().spans[0];
-									item_ch[edge_item(e)] = concat(backedge, pop_tstack().spans[1]);
-								}
-							} else {
-								// self loops
-								assert(nxt == cur);
-								tot_self_loops++;
-								int item = alloc_item(node_type::O);
-								// Make sure the nxt is -1 as well
-								item_vs[item] = {cur, -1};
-								item_ch[edge_item(e)] = unit_list(item);
-							}
-							item_ch[vert_item(cur)] = concat(item_ch[vert_item(cur)], unit_list(edge_item(e)));
-							continue;
-						}
-
-						item_vs[edge_item(e)] = make_vs(nxt, cur_depth);
-
-						assert(lowval < cur_depth);
-
-						bool is_single = true;
-
-						// make_q_node
-						tstack_t cur_tstack;
-						if (is_tree) {
-							// The span lives on side edge_dir
-							cur_tstack = make_tstack(nxt, cur_depth, edge_item(e));
-							while (!tstack.empty() && tstack.back().top_depth >= cur_depth) {
-								node_type type;
-								if (tstack.back().top_depth > cur_depth) {
-									// This will be an S node: merge the vertex in first
-									cur_tstack = merge_tstack(pop_tstack(), cur_tstack);
-
-									type = node_type::S;
-								} else if (tstack.back().v_start == cur_tstack.v_start) {
-									// This will be a P node
-									type = node_type::P;
-								} else {
-									type = node_type::R;
-								}
-								auto nxt_tstack = pop_tstack();
-								int item = maybe_unwrap(nxt_tstack, type);
-								cur_tstack = merge_tstack(nxt_tstack, cur_tstack);
-								finish_tstack(cur_tstack, item);
-							}
-							while (cur_tstack.first_idx > first_occurrence[cur_depth]) {
-								is_single = false;
-								cur_tstack = merge_tstack(pop_tstack(), cur_tstack);
-							}
-
-							if (has_return_edge || is_type_1) {
-								// NB: tstack[orig_size] is the vertex and tstack[orig_size+1] is the backedge; maybe we should reverse them?
-								assert(int(tstack.size()) >= orig_tstack + 2);
-
-								int item;
-								if (is_type_1) {
-									assert(int(tstack.size()) == orig_tstack + 2);
-
-									auto nxt_tstack = pop_tstack();
-									item = maybe_unwrap(nxt_tstack, is_single ? node_type::S : node_type::R);
-									cur_tstack = merge_tstack(nxt_tstack, cur_tstack);
-
-									cur_tstack = merge_tstack(pop_tstack(), cur_tstack);
-								} else {
-									// Just to silence a warning
-									item = -1;
-
-									while (int(tstack.size()) > orig_tstack) {
-										cur_tstack = merge_tstack(pop_tstack(), cur_tstack);
-									}
-								}
-
-								cur_tstack.v_start = cur;
-								assert(cur_tstack.top_depth == lowval);
-
-								// Fold everything to the correct side now that we're leaving the child.
-								// The entire subtree should go to the !edge_dir side.
-								cur_tstack.spans = set_sides(!edge_dir, concat(cur_tstack.spans[0], cur_tstack.spans[1]), {});
-
-								// TODO: There's some planarity folding to do here
-
-								if (is_type_1) {
-									finish_tstack(cur_tstack, item);
-									is_single = true;
-								}
-							}
-						} else {
-							assert(is_type_1);
-							// The span lives on side !edge_dir
-							cur_tstack = make_tstack(cur, lowval, edge_item(e));
-							nxt_edge_idx++;
-							setmin(first_occurrence[lowval], cur_tstack.first_idx);
-						}
-
-						// NB: We can do this check in lots of ways, maybe there's a cleaner check
-						if (is_type_1 && has_return_edge && tstack.back().v_start == cur && tstack.back().top_depth == lowval) {
-							// This will be a P node
-							auto nxt_tstack = pop_tstack();
-							int item = maybe_unwrap(nxt_tstack, node_type::P);
-							cur_tstack = merge_tstack(nxt_tstack, cur_tstack);
-							finish_tstack(cur_tstack, item);
-						}
-
-						tstack.push_back(cur_tstack);
-
-						if (!has_return_edge) {
-							// Throw cur_vert_node onto the tstack so it'll get interleaved correctly
-							tstack_t cur_vert_node = make_tstack(cur, cur_depth, vert_item(cur));
-
-							assert(!tstack.empty());
-							if (is_type_1) {
-								// Insert it underneath the backedge
-								cur_vert_node.first_idx = tstack.back().first_idx;
-								std::swap(tstack.back(), cur_vert_node);
-								tstack.push_back(cur_vert_node);
-							} else if (is_single) {
-								// This could be an S node, so leave it separate
-								tstack.push_back(cur_vert_node);
-							} else {
-								// Just eagerly merge it to avoid a later bad finish_tstack
-								tstack.back() = merge_tstack(tstack.back(), cur_vert_node);
-							}
-							has_return_edge = true;
-						}
-					}
-					if (!has_return_edge) {
+				};
+				auto pop_vert = [&]() -> void {
+					int cur_depth = int(stk.size()) - 1;
+					auto& s = stk.back();
+					int cur = stack_verts[cur_depth];
+					assert(s.ch_idx == outedges.bounds[cur+1]);
+					if (!s.has_return_edge) {
 						// Either our parent is a bridge edge, or we're just a root.
 						// We'll just leave it on tstack for future cleanup, it'll just get popped of immediately.
 						// edge_dir == !stack_dir[lowval == cur_depth - 1] == true
 						stack_dir[cur_depth] = true;
 						tstack.push_back(make_tstack(cur, cur_depth, vert_item(cur)));
 					}
-				}(rt, 0);
+					stk.pop_back();
+				};
+				// return true means jump to start_edge, return false means jump to finish_edge
+				auto start_edge = [&]() -> bool {
+					int cur_depth = int(stk.size()) - 1;
+					auto& s = stk.back();
+					int cur = stack_verts[cur_depth];
+					if (s.ch_idx == outedges.bounds[cur+1]) {
+						pop_vert();
+						return false;
+					}
+					auto [_, nxt, e, key] = outedges.dat[s.ch_idx];
+					int lowval = key / 3 - 2; if (lowval < 0) lowval = cur_depth + ~lowval;
+					int kind = key % 3;
+					bool is_tree = kind != 1;
+					[[maybe_unused]] bool is_type_1 = kind <= 1;
 
+					// edge_dir convention: false is forwards, true is backwards.
+					// That means that cur is on the edge_dir side and nxt is on the !edge_dir side.
+					stack_dir[cur_depth] = (lowval >= cur_depth ? false : !stack_dir[lowval]);
+
+					s.orig_tstack = int(tstack.size());
+					if (is_tree) {
+						first_occurrence[cur_depth] = NE;
+						push_vert(nxt);
+						return true;
+					} else {
+						return false;
+					}
+				};
+				auto finish_edge = [&]() -> void {
+					int cur_depth = int(stk.size()) - 1;
+					auto& s = stk.back();
+					int cur = stack_verts[cur_depth];
+					assert(s.ch_idx < outedges.bounds[cur+1]);
+
+					auto [_, nxt, e, key] = outedges.dat[s.ch_idx];
+					s.ch_idx++;
+
+					int lowval = key / 3 - 2; if (lowval < 0) lowval = cur_depth + ~lowval;
+					const int kind = key % 3;
+					const bool is_tree = kind != 1;
+					const bool is_type_1 = kind <= 1;
+
+					const int orig_tstack = s.orig_tstack;
+					const bool has_return_edge = s.has_return_edge;
+					const bool edge_dir = stack_dir[cur_depth];
+
+					if (lowval >= cur_depth) {
+						item_vs[edge_item(e)] = {cur, -1};
+						tot_blocks++;
+						if (is_tree) {
+							// Bridges and components
+							if (lowval == cur_depth + 1) {
+								// tstack.back() is currently just smuggling out the child vertex, prepend the bridge component
+								// This is just a shortcut for allocating a full I-type tstack
+								int item = alloc_item(node_type::I);
+								item_vs[item] = make_vs(nxt, cur_depth);
+								item_ch[edge_item(e)] = concat(unit_list(item), pop_tstack().spans[1]);
+							} else {
+								// tstack.end()[-2] is the vertex and tstack.end()[-1] is the backedge
+								auto backedge = pop_tstack().spans[0];
+								item_ch[edge_item(e)] = concat(backedge, pop_tstack().spans[1]);
+							}
+						} else {
+							// self loops
+							assert(nxt == cur);
+							tot_self_loops++;
+							int item = alloc_item(node_type::O);
+							// Make sure the nxt is -1 as well
+							item_vs[item] = {cur, -1};
+							item_ch[edge_item(e)] = unit_list(item);
+						}
+						item_ch[vert_item(cur)] = concat(item_ch[vert_item(cur)], unit_list(edge_item(e)));
+						return;
+					}
+
+					item_vs[edge_item(e)] = make_vs(nxt, cur_depth);
+
+					assert(lowval < cur_depth);
+
+					bool is_single = true;
+
+					// make_q_node
+					tstack_t cur_tstack;
+					if (is_tree) {
+						// The span lives on side edge_dir
+						cur_tstack = make_tstack(nxt, cur_depth, edge_item(e));
+						while (!tstack.empty() && tstack.back().top_depth >= cur_depth) {
+							node_type type;
+							if (tstack.back().top_depth > cur_depth) {
+								// This will be an S node: merge the vertex in first
+								cur_tstack = merge_tstack(pop_tstack(), cur_tstack);
+
+								type = node_type::S;
+							} else if (tstack.back().v_start == cur_tstack.v_start) {
+								// This will be a P node
+								type = node_type::P;
+							} else {
+								type = node_type::R;
+							}
+							auto nxt_tstack = pop_tstack();
+							int item = maybe_unwrap(nxt_tstack, type);
+							cur_tstack = merge_tstack(nxt_tstack, cur_tstack);
+							finish_tstack(cur_tstack, item);
+						}
+						while (cur_tstack.first_idx > first_occurrence[cur_depth]) {
+							is_single = false;
+							cur_tstack = merge_tstack(pop_tstack(), cur_tstack);
+						}
+
+						if (has_return_edge || is_type_1) {
+							// NB: tstack[orig_size] is the vertex and tstack[orig_size+1] is the backedge; maybe we should reverse them?
+							assert(int(tstack.size()) >= orig_tstack + 2);
+
+							int item;
+							if (is_type_1) {
+								assert(int(tstack.size()) == orig_tstack + 2);
+
+								auto nxt_tstack = pop_tstack();
+								item = maybe_unwrap(nxt_tstack, is_single ? node_type::S : node_type::R);
+								cur_tstack = merge_tstack(nxt_tstack, cur_tstack);
+
+								cur_tstack = merge_tstack(pop_tstack(), cur_tstack);
+							} else {
+								// Just to silence a warning
+								item = -1;
+
+								while (int(tstack.size()) > orig_tstack) {
+									cur_tstack = merge_tstack(pop_tstack(), cur_tstack);
+								}
+							}
+
+							cur_tstack.v_start = cur;
+							assert(cur_tstack.top_depth == lowval);
+
+							// Fold everything to the correct side now that we're leaving the child.
+							// The entire subtree should go to the !edge_dir side.
+							cur_tstack.spans = set_sides(!edge_dir, concat(cur_tstack.spans[0], cur_tstack.spans[1]), {});
+
+							// TODO: There's some planarity folding to do here
+
+							if (is_type_1) {
+								finish_tstack(cur_tstack, item);
+								is_single = true;
+							}
+						}
+					} else {
+						assert(is_type_1);
+						// The span lives on side !edge_dir
+						cur_tstack = make_tstack(cur, lowval, edge_item(e));
+						nxt_edge_idx++;
+						setmin(first_occurrence[lowval], cur_tstack.first_idx);
+					}
+
+					// NB: We can do this check in lots of ways, maybe there's a cleaner check
+					if (is_type_1 && has_return_edge && tstack.back().v_start == cur && tstack.back().top_depth == lowval) {
+						// This will be a P node
+						auto nxt_tstack = pop_tstack();
+						int item = maybe_unwrap(nxt_tstack, node_type::P);
+						cur_tstack = merge_tstack(nxt_tstack, cur_tstack);
+						finish_tstack(cur_tstack, item);
+					}
+
+					tstack.push_back(cur_tstack);
+
+					if (!has_return_edge) {
+						// Throw cur_vert_node onto the tstack so it'll get interleaved correctly
+						tstack_t cur_vert_node = make_tstack(cur, cur_depth, vert_item(cur));
+
+						assert(!tstack.empty());
+						if (is_type_1) {
+							// Insert it underneath the backedge
+							cur_vert_node.first_idx = tstack.back().first_idx;
+							std::swap(tstack.back(), cur_vert_node);
+							tstack.push_back(cur_vert_node);
+						} else if (is_single) {
+							// This could be an S node, so leave it separate
+							tstack.push_back(cur_vert_node);
+						} else {
+							// Just eagerly merge it to avoid a later bad finish_tstack
+							tstack.back() = merge_tstack(tstack.back(), cur_vert_node);
+						}
+						s.has_return_edge = true;
+					}
+				};
+
+				push_vert(rt);
+				bool is_start = true;
+				while (!stk.empty()) {
+					if (is_start) {
+						is_start = start_edge();
+					} else {
+						finish_edge();
+						is_start = true;
+					}
+				}
+				assert(!is_start);
 				auto component_val = pop_tstack();
 				item_ch[ROOT_ITEM] = concat(item_ch[ROOT_ITEM], component_val.spans[1]);
 			}
